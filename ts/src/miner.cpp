4f8bd729dae14fc57b9970012a2ea1762c56207b
Fatih BAKIR <bakir@ucsb.edu>
2018-02-15 00:37:15 -0800

    fibonacci return value

--- ts/src/miner.cpp
+++ ts/src/miner.cpp
@@ -1,17 +1,9 @@
-/*********************************************************************
- * NAN - Native Abstractions for Node.js
- *
- * Copyright (c) 2015 NAN contributors
- *
- * MIT License <https://github.com/nodejs/nan/blob/master/LICENSE.md>
- ********************************************************************/
-
 #include "miner.hpp"
 #include "bind.hpp"
 
 using namespace meta;
 
-auto fibonacci(int n)
+int fibonacci(int n)
 {
 	if (n <= 1) return n;
 	return fibonacci(n - 1) + fibonacci(n - 2);
@@ -21,7 +13,7 @@ auto foo(std::string x, int y)
 {
 	// this block is executed in a thread
 	auto res = fibonacci(y);	
-	
+
 	return [=](auto cb){
 		// this block is executed in the node event loop
 		call(cb, x + x, res);
