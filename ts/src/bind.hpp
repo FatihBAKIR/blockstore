b84c07a205cfe31a1a5187c2f01b08dcdc4b4d79
Fatih BAKIR <bakir@ucsb.edu>
2018-02-15 01:16:42 -0800

    cleanup

--- ts/src/bind.hpp
+++ ts/src/bind.hpp
@@ -16,18 +16,6 @@
 
 namespace meta
 {
-	using v8::Function;
-	using v8::Local;
-	using v8::Number;
-	using v8::Value;
-	using Nan::AsyncQueueWorker;
-	using Nan::AsyncWorker;
-	using Nan::Callback;
-	using Nan::HandleScope;
-	using Nan::New;
-	using Nan::Null;
-	using Nan::To;
-
 	template <class T> struct type {};
 
 	using clval = const v8::Local<v8::Value>&; 
@@ -36,21 +24,21 @@ namespace meta
 	{ return *v8::String::Utf8Value(t.As<v8::String>()); }
 
 	int extract(type<int>, clval t)
-	{ return To<int>(t).FromJust(); }
+	{ return Nan::To<int>(t).FromJust(); }
 
 	float extract(type<float>, clval t)
-	{ return To<double>(t).FromJust(); }
+	{ return Nan::To<double>(t).FromJust(); }
 
 	double extract(type<double>, clval t)
-	{ return To<double>(t).FromJust(); }
+	{ return Nan::To<double>(t).FromJust(); }
 
 	template <class T>
 	struct map {};
 
-	template <> struct map<int> 		{ using type = Number; 		};
-	template <> struct map<long> 		{ using type = Number; 		};
-	template <> struct map<float> 		{ using type = Number; 		};
-	template <> struct map<double> 		{ using type = Number; 		};
+	template <> struct map<int> 		{ using type = v8::Number; 	};
+	template <> struct map<long> 		{ using type = v8::Number; 	};
+	template <> struct map<float> 		{ using type = v8::Number; 	};
+	template <> struct map<double> 		{ using type = v8::Number; 	};
 	template <> struct map<std::string> { using type = v8::String; 	};
 
 	template <class T>
@@ -62,7 +50,7 @@ namespace meta
 	template <class T>
 	struct mapping_traits
 	{
-		using new_t = clean_t<decltype(New<map_t<T>>(std::declval<T>()))>;
+		using new_t = clean_t<decltype(Nan::New<map_t<T>>(std::declval<T>()))>;
 	};
 
 	template <class...> struct list {};
@@ -87,8 +75,9 @@ namespace meta
 	};
 
 	template <class T>
-	Local<Value> convert(T&& t)
+	v8::Local<v8::Value> convert(T&& t)
 	{
+		using Nan::New;
 		using traits = mapping_traits<T>;
 		using new_t = typename traits::new_t;
 		if constexpr(std::is_same<new_t, v8::MaybeLocal<map_t<T>>>{})
@@ -102,19 +91,19 @@ namespace meta
 	}
 
 	template <class... Ts>
-	void call(Callback* cb, Ts&&... ts)
+	void call(Nan::Callback* cb, Ts&&... ts)
 	{
-		HandleScope scope;
-		Local<Value> argv[] = { convert(std::forward<Ts>(ts))... };
+		Nan::HandleScope scope;
+		v8::Local<v8::Value> argv[] = { convert(std::forward<Ts>(ts))... };
 		cb->Call(sizeof...(Ts), argv);
 	}
 
 	template <class FunT, class... ArgTs>
-	class MetaWorker : public AsyncWorker
+	class MetaWorker : public Nan::AsyncWorker
 	{
 		using ret_t = clean_t<decltype(std::declval<FunT>()(std::declval<ArgTs>()...))>;
 	public:
-		MetaWorker(Callback* cb, FunT fun, std::tuple<ArgTs...> args) 
+		MetaWorker(Nan::Callback* cb, FunT fun, std::tuple<ArgTs...> args) 
 			: AsyncWorker(cb), m_fun(fun), m_args(std::move(args))
 		{}
 
@@ -145,17 +134,10 @@ namespace meta
 	{
 		constexpr auto arg_count = sizeof...(ArgTs);
 		auto arg_tuple = std::tuple<ArgTs...>{ extract(type<ArgTs>{}, info[I])... };
-		Callback *callback = new Callback(info[arg_count].As<Function>());
+		auto callback = new Nan::Callback(info[arg_count].As<v8::Function>());
 		auto res = new MetaWorker<FunT, ArgTs...>{callback, fun, std::move(arg_tuple)};
 		AsyncQueueWorker(res);
 	}
-
-	/*template<class... ArgTs, class FunT>
-	auto bind(FunT&& fun, const Nan::FunctionCallbackInfo<v8::Value>& info)
-	{
-		constexpr auto arg_count = sizeof...(ArgTs);
-		return bind<ArgTs...>(std::forward<FunT>(fun), info, std::make_index_sequence<arg_count>{});
-	}*/
 
 	template<class FunT>
 	auto bind(FunT&& fun, const Nan::FunctionCallbackInfo<v8::Value>& info)
